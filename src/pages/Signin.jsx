e06642551e07f37365582b918b66082927122c7c
faabfab <faab.fab@free.fr>
2024-04-19 18:57:40 +0200

    login regex

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { useState } from "react";
+import { validEmail, validPassword } from "./../components/tools/Regex";
 
 function Signin() {
-  const userId = 1;
-  const userLink = "/user/" + userId;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailErr, setEmailErr] = useState(false);
+  const [pwdError, setPwdError] = useState(false);
+  const validate = () => {
+    if (!validEmail.test(email)) {
+      setEmailErr(true);
+    }
+    if (!validPassword.test(password)) {
+      setPwdError(true);
+    }
+  };
+
   return (
     <React.StrictMode>
       <main className="main bg-dark">
@@ -12,20 +25,32 @@ function Signin() {
           <h1>Sign In</h1>
           <form>
             <div className="input-wrapper">
-              <label for="username">Username</label>
-              <input type="text" id="username" />
+              <label htmlFor="username">Email</label>
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="input-wrapper">
-              <label for="password">Password</label>
-              <input type="password" id="password" />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
             <div className="input-remember">
               <input type="checkbox" id="remember-me" />
-              <label for="remember-me">Remember me</label>
+              <label htmlFor="remember-me">Remember me</label>
             </div>
-            <Link to={userLink} className="sign-in-button">
+            <button className="sign-in-button" onClick={validate}>
               Sign In
-            </Link>
+            </button>
+            {emailErr && <p>Your email is invalid</p>}
+            {pwdError && <p>Your password is invalid</p>}
           </form>
         </section>
       </main>
